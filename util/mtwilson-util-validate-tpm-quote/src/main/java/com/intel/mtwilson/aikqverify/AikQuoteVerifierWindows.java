7a77459ac0b2f513a367665bcc1b89b5602870e2
Arijit Ghosh <arijit.ghosh@intel.com>
2019-04-23 16:51:28 +0530

    Removed unnecessary logs

--- util/mtwilson-util-validate-tpm-quote/src/main/java/com/intel/mtwilson/aikqverify/AikQuoteVerifierWindows.java
+++ util/mtwilson-util-validate-tpm-quote/src/main/java/com/intel/mtwilson/aikqverify/AikQuoteVerifierWindows.java
@@ -9,44 +9,39 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.security.*;
-import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 
 public class AikQuoteVerifierWindows {
 
     private static final transient org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AikQuoteVerifierWindows.class);
+    private static final int SHA1_SIZE = 20;
+    private static final int MAX_NUM_OF_PCRS = 24;
 
-    public String verifyAIKQuoteWindows(byte[] challenge, byte[] quoteFileContent, PublicKey rsaPublicKey) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+    public String verifyAIKQuoteWindows(byte[] challenge, byte[] quoteFileContent, PublicKey rsaPublicKey) throws IOException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 
 
-        byte[] quote = null;
-        byte[] pcrs = null;
-        byte[] signature = null;
-        byte[] chalmd = new byte[20];
+        byte[] quote;
+        byte[] pcrs;
+        byte[] signature;
+        byte[] chalmd;
         byte[] qinfoDigest;
         byte[] quoteNonce;
         byte[] pcrSelect;
-        byte[] compositeHash = new byte[20];
-        byte[] pcrConcat = new byte[20 * 24];
-        byte[] pcrComposite = new byte[20 * 24 + 9];
-        StringBuilder sb = new StringBuilder();
+        byte[] compositeHash;
+        byte[] pcrConcat = new byte[SHA1_SIZE * MAX_NUM_OF_PCRS];
+        byte[] pcrComposite;
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int selectLength = 0;
-        int pcrLength = 0;
+        int selectLength;
+        int pcrLength;
         int pcr;
         int pcrBytesIndex = 0;
+        int concatLength = 0;
+        int numOfPCRNotIncluded = 0;
         String pcrValues = "";
 
-
-        messageDigest.update(challenge);
-        chalmd = messageDigest.digest();
-        messageDigest.reset();
-        for (byte b : chalmd) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("Challenge message digest hex: {}", sb.toString());
-        sb = new StringBuilder();
+        chalmd = messageDigest.digest(challenge);
+        log.debug("Challenge message digest: {}", chalmd);
 
 
         int headerLength = ByteBuffer.wrap(quoteFileContent, 8, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
@@ -58,46 +53,30 @@ public class AikQuoteVerifierWindows {
         int signatureLength = ByteBuffer.wrap(quoteFileContent, 20, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
         log.debug("Signature length: {}", signatureLength);
         pcrs = Arrays.copyOfRange(quoteFileContent, headerLength, headerLength + pcrLength);
-        for (byte b : pcrs) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("PCR hex: {}", sb.toString());
-        sb = new StringBuilder();
+
+        log.debug("PCR info: {}", pcrs);
 
         quote = Arrays.copyOfRange(quoteFileContent, headerLength + pcrLength, headerLength + pcrLength + quoteLength);
-        for (byte b : quote) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("Quote hex: {}", sb.toString());
-        sb = new StringBuilder();
+        log.debug("Quote: {}", quote);
 
         quoteNonce = Arrays.copyOfRange(quote, 6, 26);
-        for (byte b : quoteNonce) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("Quote nonce: {}", sb.toString());
-        sb = new StringBuilder();
+
+        log.debug("Quote nonce read from file");
 
         if (!Arrays.equals(chalmd, quoteNonce)) {
             log.error("Nonce in quote does not match nonce in challenge file");
             throw new IOException("Nonce in quote does not match nonce in challenge file");
         }
         qinfoDigest = messageDigest.digest(quote);
-        for (byte b : qinfoDigest) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("Quote digest hex: {}", sb.toString());
-        sb = new StringBuilder();
+
+        log.debug("Quote digest: {}", qinfoDigest);
+
         signature = Arrays.copyOfRange(quoteFileContent, headerLength + pcrLength + quoteLength, headerLength + pcrLength + quoteLength + signatureLength);
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, rsaPublicKey);
         byte[] decryptedSignature = cipher.doFinal(signature);
 
-        for (byte b : decryptedSignature) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("Decrypted signature hex: {}", sb.toString());
-        sb = new StringBuilder();
+        log.debug("Signature has been decrypted");
 
         if (!Arrays.equals(qinfoDigest, Arrays.copyOfRange(decryptedSignature, decryptedSignature.length - qinfoDigest.length, decryptedSignature.length))) {
             log.error("Quote verification failed");
@@ -107,20 +86,13 @@ public class AikQuoteVerifierWindows {
         selectLength = ByteBuffer.wrap(quote, 6 + 20, 2).getShort();
         log.debug("Select length: {}", selectLength);
         pcrSelect = Arrays.copyOfRange(quote, 6 + 20 + 2, 6 + 20 + 2 + selectLength);
-        for (byte b : pcrSelect) {
-            sb.append(String.format(" %02x ", b));
-        }
+
         log.debug("PCR select: {}", pcrSelect);
-        log.debug("PCR select hex: {}", sb.toString());
-        sb = new StringBuilder();
+
         compositeHash = Arrays.copyOfRange(quote, 6 + 20 + 2 + selectLength + 1, 6 + 20 + 2 + selectLength + 1 + 20);
-        for (byte b : compositeHash) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("Composite hex: {}", sb.toString());
-        sb = new StringBuilder();
-        int concatLength = 0;
-        int numOfPCRNotIncluded = 0;
+
+        log.debug("Composite hash has been created");
+
         for (pcr = 0; pcr < 8 * selectLength; pcr++) {
             if ((pcrSelect[pcr / 8] & (1 << (pcr % 8))) != 0) {
                 if (concatLength < pcrConcat.length) {
@@ -133,11 +105,8 @@ public class AikQuoteVerifierWindows {
                 numOfPCRNotIncluded++;
             }
         }
-        for (byte b : pcrConcat) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("PCR concat hex: {}", sb.toString());
-        sb = new StringBuilder();
+
+        log.debug("PCR concat: {}", pcrConcat);
 
         log.debug("Concat length: {}", concatLength);
         ByteBuffer bb = ByteBuffer.allocate(4);
@@ -147,18 +116,11 @@ public class AikQuoteVerifierWindows {
         baos.write(pcrConcat);
         pcrComposite = baos.toByteArray();
 
-        for (byte b : pcrComposite) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("PCR composite hex: {}", sb.toString());
-        sb = new StringBuilder();
+        log.debug("PCR composite: {}", pcrComposite);
 
         byte[] pcrDigest = messageDigest.digest(Arrays.copyOfRange(pcrComposite, 0, 2 + selectLength + 4 + concatLength));
-        for (byte b : pcrDigest) {
-            sb.append(String.format(" %02x ", b));
-        }
-        log.debug("PCR digest hex: {}", sb.toString());
-        sb = new StringBuilder();
+
+        log.debug("PCR digest: {}", pcrDigest);
 
         if (!Arrays.equals(pcrDigest, compositeHash)) {
             log.error("PCR digest verification failed");
