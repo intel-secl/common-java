23db71d2154c15ef6d78a2fe7c3e7650956373d7
pulivenx <venkata.avinashx.puli@intel.com>
2019-05-02 02:07:28 -0700

    Fix for #ISECL-3936 PSAV:Arbitrary Host Header Requests Accepted

---
+++ features/mtwilson-http-security/src/main/java/com/intel/mtwilson/security/http/ArbitraryHostRequestFilter.java
@@ -0,0 +1,48 @@
+package com.intel.mtwilson.security.http;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.Context;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+public class ArbitraryHostRequestFilter implements ContainerRequestFilter {
+
+    private static Logger log = LoggerFactory.getLogger(ArbitraryHostRequestFilter.class);
+    
+    @Context
+    private HttpServletRequest request;
+
+    @Override
+    public void filter(ContainerRequestContext containerRequest)
+            throws WebApplicationException {
+            
+        String hName = request.getServerName();
+        String lName = request.getLocalName();
+        String hIP = null;
+        try {
+            hIP = InetAddress.getByName(hName).getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        if (!lName.equals(hIP)) {
+            containerRequest.abortWith(Response.status(Response.Status.FORBIDDEN)
+                    .entity("Invalid Request")
+                    .build());
+            log.error("Invalid host header, dropping request from "+ request.getRemoteHost()); 
+            return;         
+        }
+        
+    }
+}
