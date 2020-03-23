1287dda23819821c8bcb042247895b19907f7d37
R, Praveenraj <praveenraj.r@intel.com>
2020-03-23 01:50:40 -0700

    added clear function

--- util/mtwilson-util-crypto-key/src/main/java/com/intel/mtwilson/util/crypto/key2/KMIPCipherKey.java
+++ util/mtwilson-util-crypto-key/src/main/java/com/intel/mtwilson/util/crypto/key2/KMIPCipherKey.java
@@ -7,13 +7,25 @@ package com.intel.mtwilson.util.crypto.key2;
 
 import com.intel.mtwilson.util.crypto.key2.CipherKey;
 
+import java.util.Arrays;
+
 public class KMIPCipherKey extends CipherKey {
     private String kmipKeyUUID;
+
     public void setKMIPKeyUUID(String kmipKeyUUID){
         this.kmipKeyUUID = kmipKeyUUID;
     }
 
     public String getKmipKeyUUID(){
         return this.kmipKeyUUID;
+    }
+
+    public void clear() {
+        setAlgorithm(null);
+        setKeyId(null);
+        setKeyLength(null);
+        setMode(null);
+        setPaddingMode(null);
+        setKMIPKeyUUID(null);
     }
 }
